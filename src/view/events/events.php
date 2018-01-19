281e975db27bb34199b658c59c0a55674b700d03
ImLuze <luzedz@gmail.com>
2018-01-19 19:21:18 +0100

    Added basic small card

--- src/view/events/events.php
+++ src/view/events/events.php
@@ -1,22 +1,38 @@
 <section>
-  <h1>Events</h1>
   <?php foreach($events as $event): ?>
-    <article>
+    <!-- <article>
       <header><h2><?php echo $event['title']; ?></h2></header>
       <dl>
-        <!--<dt>code</dt><dd><?php echo $event['code'];?></dd>
+        <dt>code</dt><dd><?php echo $event['code'];?></dd>
         <dt>content</dt><dd><?php echo $event['content'];?></dd>
         <dt>practical</dt><dd><?php echo $event['practical'];?></dd>
       <dt>location</dt><dd><?php echo $event['location'];?></dd>
         <dt>address</dt><dd><?php echo $event['address'];?></dd>
-        <dt>postal</dt><dd><?php echo $event['postal'];?></dd>-->
+        <dt>postal</dt><dd><?php echo $event['postal'];?></dd>
         <dt>city</dt><dd><?php echo $event['city'];?></dd>
         <dt>start</dt><dd><?php echo $event['start'];?></dd>
         <dt>end</dt><dd><?php echo $event['end'];?></dd>
-          <!--<dt>link</dt><dd><?php echo $event['link'];?></dd>
+          <dt>link</dt><dd><?php echo $event['link'];?></dd>
         <dt>organisers</dt><dd><ul><?php foreach($event['organisers'] as $organiser): ?><li><?php echo $organiser['name'];?></li><?php endforeach;?></ul></dd>
         <dt>tags</dt><dd><ul><?php foreach($event['tags'] as $tag): ?><li><?php echo $tag['tag'];?></li><?php endforeach;?></ul></dd>
-      --></dl>
-    </article>
+      </dl>
+    </article> -->
   <? endforeach;?>
+  <section class="cards">
+    <article class="card-small">
+      <div class="timestamp">
+        <p>16</p>
+        <p>sept</p>
+      </div>
+      <h2>Mobiliteitsmarkt Antwerpen Autovrij</h2>
+      <div class="card-tags">
+        <p class="tag">markt</p>
+        <p class="tag">fiets</p>
+        <p class="tag">bakfiets</p>
+        <p class="tag">deelfietsen</p>
+      </div>
+      <p>Nieuwsgierig naar de allernieuwste trends op het vlak van vervoer of autodelen? Of wilt u meer weten over de</p>
+      <a class="read-more" href="#">Meer info</a>
+    </article>
+  </section>
 </section>
