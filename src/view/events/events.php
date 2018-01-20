9de13c95928bca6cd940494078c42ed119117c3f
ImLuze <luzedz@gmail.com>
2018-01-20 22:20:37 +0100

    Added basic date filter

--- src/view/events/events.php
+++ src/view/events/events.php
@@ -19,6 +19,36 @@
     </article> -->
   <? endforeach;?>
   <section class="cards">
+    <ul class="date-filter">
+      <li class="date-filter-day">
+        <p>16</p>
+        <p>september</p>
+      </li>
+      <li class="date-filter-day date-filter-day-active">
+        <p>17</p>
+        <p>september</p>
+      </li>
+      <li class="date-filter-day">
+        <p>18</p>
+        <p>september</p>
+      </li>
+      <li class="date-filter-day">
+        <p>19</p>
+        <p>september</p>
+      </li>
+      <li class="date-filter-day">
+        <p>20</p>
+        <p>september</p>
+      </li>
+      <li class="date-filter-day">
+        <p>21</p>
+        <p>september</p>
+      </li>
+      <li class="date-filter-day">
+        <p>22</p>
+        <p>september</p>
+      </li>
+    </ul>
     <?php foreach($events as $event): ?>
       <?php if(file_exists('./assets/img/photos/'.$event['code'])): ?>
         <article class="card-photo">
@@ -71,71 +101,5 @@
         </article>
       <?php endif; ?>
     <? endforeach;?>
-    <!-- <article class="card-small">
-      <div class="timestamp">
-        <p>16</p>
-        <p>sept</p>
-      </div>
-      <h2>Mobiliteitsmarkt Antwerpen Autovrij</h2>
-      <div class="card-tags">
-        <p class="tag">markt</p>
-        <p class="tag">fiets</p>
-        <p class="tag">bakfiets</p>
-        <p class="tag">deelfietsen</p>
-      </div>
-      <p class="card-bodycopy">Nieuwsgierig naar de allernieuwste trends op het vlak van vervoer of autodelen? Of wilt u meer weten over de</p>
-      <a class="read-more" href="#">Meer info</a>
-    </article>
-    <article class="card-photo">
-      <div class="timestamp">
-        <p>16</p>
-        <p>sept</p>
-      </div>
-      <div class="card-img">
-        <img src="./assets/img/ANT1/Blue-bike-stad.jpg" alt="Blue bike stad">
-        <h2>Mobiliteitsmarkt Antwerpen Autovrij</h2>
-      </div>
-      <div class="card-tags">
-        <p class="tag">markt</p>
-        <p class="tag">fiets</p>
-        <p class="tag">bakfiets</p>
-        <p class="tag">deelfietsen</p>
-      </div>
-      <p class="card-bodycopy">Nieuwsgierig naar de allernieuwste trends op vlak van vervoer en autodelen? Of wilt u meer info over de wegenwerken en slimme alternatieven in Antwerpen? Neem dan zeker ook een kijkje op de mobiliteitsmarkt.</p>
-      <a class="read-more" href="#">Meer info</a>
-    </article>
-    <article class="card-photo">
-      <div class="timestamp">
-        <p>16</p>
-        <p>sept</p>
-      </div>
-      <div class="card-img">
-        <img src="./assets/img/ANT1/Blue-bike-stad.jpg" alt="Blue bike stad">
-        <h2>Mobiliteitsmarkt Antwerpen Autovrij</h2>
-      </div>
-      <div class="card-tags">
-        <p class="tag">markt</p>
-        <p class="tag">fiets</p>
-        <p class="tag">bakfiets</p>
-        <p class="tag">deelfietsen</p>
-      </div>
-      <p class="card-bodycopy">Nieuwsgierig naar de allernieuwste trends op vlak van vervoer en autodelen? Of wilt u meer info over de wegenwerken en slimme alternatieven in Antwerpen? Neem dan zeker ook een kijkje op de mobiliteitsmarkt.</p>
-      <a class="read-more" href="#">Meer info</a>
-    </article>
-    <article class="card-small">
-      <div class="timestamp">
-        <p>16</p>
-        <p>sept</p>
-      </div>
-      <h2>Mobiliteitsmarkt Antwerpen Autovrij</h2>
-      <div class="card-tags">
-        <p class="tag">markt</p>
-        <p class="tag">fiets</p>
-        <p class="tag">bakfiets</p>
-        <p class="tag">deelfietsen</p>
-      </div>
-      <p class="card-bodycopy">Nieuwsgierig naar de allernieuwste trends op het vlak van vervoer of autodelen? Of wilt u meer weten over de</p>
-      <a class="read-more" href="#">Meer info</a>
-    </article> -->
   </section>
 </section>
