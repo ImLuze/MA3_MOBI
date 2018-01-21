cf62251d6f140f80be6303a07765a5a56df5ce9b
ImLuze <luzedz@gmail.com>
2018-01-21 13:25:43 +0100

    Date filter fixed when on top

--- src/js/script.js
+++ src/js/script.js
@@ -12,7 +12,7 @@ const checkActiveLink = () => {
   selector[0].style.left = `${activeLink.x}px`;
 };
 
-const init = () => {
+const setupHoverFollower = () => {
   const links = document.querySelectorAll(`.navigation-link`);
   const navigation = document.querySelectorAll(`.navigation`);
   checkActiveLink();
@@ -21,6 +21,55 @@ const init = () => {
     links[i].addEventListener(`mouseover`, handleLinkHover);
   }
   navigation[0].addEventListener(`mouseleave`, checkActiveLink);
+};
+
+// const isInViewport = (e, elem) => {
+//   const rect = elem.getBoundingClientRect();
+//   const html = document.documentElement;
+//
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= ((window.innerHeight) || (html.clientHeight)) &&
+//     rect.right <= ((window.innerWidth) || (html.clientWidth))
+//   );
+// };
+
+const isAtTop = (bottomEl, topEl, distance) => {
+  const bottomElRect = bottomEl.getBoundingClientRect();
+  const topElRect = topEl.getBoundingClientRect();
+  // console.log(bottomElRect.top, topElRect.bottom);
+  if(bottomElRect.top <= (topElRect.bottom + distance)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const setupFixedDayFilter = () => {
+  const filter = document.querySelectorAll(`.date-filter`)[0];
+  const nav = document.querySelectorAll(`.navigation`)[0];
+  const container = document.querySelectorAll(`.cards`)[0];
+  const firstPhotoCard = document.querySelectorAll(`.card-photo`)[0];
+  // const firstSmallCard = document.querySelectorAll(`.card-small`)[0];
+
+  window.addEventListener(`scroll`, () => {
+    if(isAtTop(filter, nav, 1)) {
+      filter.classList.add(`date-filter-fixed`);
+      container.classList.add(`cards-fixed`);
+    }
+
+    if(!isAtTop(firstPhotoCard, filter, 29)) {
+      filter.classList.remove(`date-filter-fixed`);
+      container.classList.remove(`cards-fixed`);
+    }
+  });
+
+};
+
+const init = () => {
+  setupHoverFollower();
+  setupFixedDayFilter();
 };
 
 init();
