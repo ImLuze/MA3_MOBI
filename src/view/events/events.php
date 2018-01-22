efd87124a2a22a93f0781a5231d348d848acf4f4
ImLuze <luzedz@gmail.com>
2018-01-22 13:13:16 +0100

    Added map

--- src/view/events/events.php
+++ src/view/events/events.php
@@ -1,23 +1,5 @@
-<section>
-  <?php foreach($events as $event): ?>
-    <!-- <article>
-      <header><h2><?php echo $event['title']; ?></h2></header>
-      <dl>
-        <dt>code</dt><dd><?php echo $event['code'];?></dd>
-        <dt>content</dt><dd><?php echo $event['content'];?></dd>
-        <dt>practical</dt><dd><?php echo $event['practical'];?></dd>
-      <dt>location</dt><dd><?php echo $event['location'];?></dd>
-        <dt>address</dt><dd><?php echo $event['address'];?></dd>
-        <dt>postal</dt><dd><?php echo $event['postal'];?></dd>
-        <dt>city</dt><dd><?php echo $event['city'];?></dd>
-        <dt>start</dt><dd><?php echo $event['start'];?></dd>
-        <dt>end</dt><dd><?php echo $event['end'];?></dd>
-          <dt>link</dt><dd><?php echo $event['link'];?></dd>
-        <dt>organisers</dt><dd><ul><?php foreach($event['organisers'] as $organiser): ?><li><?php echo $organiser['name'];?></li><?php endforeach;?></ul></dd>
-        <dt>tags</dt><dd><ul><?php foreach($event['tags'] as $tag): ?><li><?php echo $tag['tag'];?></li><?php endforeach;?></ul></dd>
-      </dl>
-    </article> -->
-  <? endforeach;?>
+<img class="map" src="./assets/img/map.jpg" alt="belgium">
+<section class="events-container">
   <section class="cards">
     <div class="selected-tags"></div>
     <!-- <div class="tag-filter">
